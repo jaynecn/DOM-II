fe3a60655a6722a931948a369496de75bb681675
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2019-05-21 14:55:54 +0100

    got mouseover and mouseenter/mouseleave working.  Trying out a scroll function but cant get it to work

--- js/index.js
+++ js/index.js
@@ -1,1 +1,37 @@
 // Your code goes here
+let logoHeading = document.querySelector('.logo-heading');
+console.log(logoHeading);
+
+logoHeading.addEventListener('mouseenter', function() { logoHeading.style.backgroundColor = "yellow";
+});
+logoHeading.addEventListener('mouseleave', function() { logoHeading.style.backgroundColor = "white";
+});
+
+const navContainer = document.querySelector('.nav');
+console.log(navContainer);
+
+navContainer.addEventListener('click', function(element) {
+    element.target.style.backgroundColor = 'yellow';
+});
+
+const containerHome = document.getElementsByClassName('content-destination');
+console.log(containerHome);
+
+containerHome.addEventListener('scroll', function() {
+  containerHome.style.backgroundColor = 'green';
+});
+
+// const images = document.getElementsByTagName('img');
+// console.log(images);
+
+// images.addEventListener('dblclick', function(event) {
+//   event.target.style.display = 'none';
+// });
+
+
+const textLarge = document.querySelectorAll('.text-content');
+console.log(textLarge);
+
+textLarge.addEventListener('dblclick', function() {
+  textLarge.classList.toggle('.large');
+});
