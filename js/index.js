96fb9a7b5cb7c0d3fe8ecc1fd5f8385a147c7bad
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2019-05-21 17:15:00 +0100

    trying to get toggle to work

--- js/index.js
+++ js/index.js
@@ -29,12 +29,59 @@ images.addEventListener('dblclick', function() {
   images.style.display = 'none';
 });
 
+const buttons = document.querySelectorAll('.btn');
+console.log(buttons);
+
+buttons[0].classList.add('one');
+buttons[1].classList.add('two');
+buttons[2].classList.add('three');
+
+const btn1 = document.querySelector('.btn.one');
+console.log(btn1);
+
+btn1.addEventListener('dblclick', function() {
+  btn1.style.color = 'green';
+  btn1.style.backgroundColor = 'yellow';
+});
+
+const btn2 = document.querySelector('.two');
+console.log(btn2);
+
+btn2.addEventListener('dblclick', function() {
+  btn2.style.color = 'white';
+  btn2.style.backgroundColor = 'black';
+});
+
+const btn3 = document.querySelector('.three');
+console.log(btn3);
+
+btn3.addEventListener('dblclick', function() {
+  btn3.style.color = 'purple';
+  btn3.style.backgroundColor = 'red';
+});
+
+const destination = document.querySelector('.text-content');
+console.log(destination);
+
+
+function classToggle() {
+  this.classList.toggle('large');
+  this.classList.toggle('normal');
+}
+destination.addEventListener('click', classToggle);
+
+
+// document.querySelector('#div').addEventListener('click', classToggle);
+
+// destination.addEventListener('keydown', event => {
+//   if ('keydown') {
+//     return destination.style.backgroundColor = 'green';
+//     }
+// });
+
 
 
 
 // const textLarge = document.querySelectorAll('.text-content');
 // console.log(textLarge);
 
-// textLarge.addEventListener('dblclick', function() {
-//   textLarge.classList.toggle('.large');
-// });
